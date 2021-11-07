172453af7eed55754f50cdb13081cda2448156b9
Dinar Churakov <dyrelosh@gmail.com>
2021-11-07 16:01:42 +0300

    - Fix bugs

--- Dinar Churakov/Churakov_L3/app/src/main/java/com/example/design_log/validation/Variables.kt
+++
@@ -1,9 +0,0 @@
-package com.example.design_log.validation
-
-enum class VariableLenght(val lenght : Int) {
-    LENGHT_ERROR(7),
-    USERNAME_LENGHT_ERROR(4);
-}
-enum class VariableAt(val  at : String) {
-    EMAIL_ERROR("@")
-}
