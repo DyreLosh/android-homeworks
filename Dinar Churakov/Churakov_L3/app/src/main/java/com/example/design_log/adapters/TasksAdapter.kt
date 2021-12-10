925b83c5f91452735e3d7761ea8917ba46f5eda7
Dinar Churakov <dyrelosh@gmail.com>
2021-12-11 01:48:09 +0300

    Correct lint errors

--- Dinar Churakov/Churakov_L3/app/src/main/java/com/example/design_log/adapters/TasksAdapter.kt
+++ Dinar Churakov/Churakov_L3/app/src/main/java/com/example/design_log/adapters/TasksAdapter.kt
@@ -12,8 +12,9 @@ class TasksAdapter : RecyclerView.Adapter<TaskViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
         return TaskViewHolder(
-            ItemTaskBinding.inflate(LayoutInflater.from(parent.context), parent, false
-        ))
+            ItemTaskBinding.inflate( LayoutInflater.from(parent.context), parent, false
+            )
+        )
     }
 
     override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
