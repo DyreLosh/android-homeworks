c2e8a4e7cfad915f6b9e0e707905586cc0a967fb
Dinar Churakov <dyrelosh@gmail.com>
2021-12-11 01:53:49 +0300

    Correct lint errors

--- Dinar Churakov/Churakov_L3/app/src/main/java/com/example/design_log/fragments/TasksFragment.kt
+++ Dinar Churakov/Churakov_L3/app/src/main/java/com/example/design_log/fragments/TasksFragment.kt
@@ -16,7 +16,6 @@ import java.time.LocalTime
 import java.time.format.DateTimeFormatter
 import java.time.format.FormatStyle
 
-
 class TasksFragment : Fragment() {
 
     lateinit var binding: FragmentTasksBinding
