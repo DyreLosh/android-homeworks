7ab7726f9399d2bb8ab1d6f1992c8013ab5f971e
Dinar Churakov <dyrelosh@gmail.com>
2022-04-14 07:14:19 +0300

    resolve error

--- Dinar Churakov/Churakov_L3/app/src/main/java/com/example/design_log/presentation/ui/fragments/TasksFragment.kt
+++ Dinar Churakov/Churakov_L3/app/src/main/java/com/example/design_log/presentation/ui/fragments/TasksFragment.kt
@@ -9,7 +9,6 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
-import com.example.design_log.R
 import com.example.design_log.common.preference.PreferenceManager
 import com.example.design_log.data.https.ApiService
 import com.example.design_log.data.https.model.NewTask
@@ -17,10 +16,11 @@ import com.example.design_log.data.https.model.TasksResponse
 import com.example.design_log.databinding.FragmentTasksBinding
 import com.example.design_log.databinding.NewTaskDialogBinding
 import com.example.design_log.presentation.adapters.recyclerview.TasksAdapter
+import com.example.design_log.R
+import java.net.HttpURLConnection
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import java.net.HttpURLConnection
 
 class TasksFragment : Fragment() {
 
